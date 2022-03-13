1999987679f6c57f3c282afab887ec973214617d
AmirPouyaAmiri <94058945+AmirPouyaAmiri@users.noreply.github.com>
2022-03-13 13:25:04 +0330

    Delete exception fasle 4/tamrin 2 directory

--- exception fasle 4/tamrin 2/src/com/company/Main.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Main {
-
-    public static void main(String[] args) throws Moadele {
-        Moadele moadele=new Moadele();
-        moadele.zarib();
-    }
-}
