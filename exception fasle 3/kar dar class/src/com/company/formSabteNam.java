73386eb01c3b07126907fbb87bed0adddd31ca34
AmirPouyaAmiri <94058945+AmirPouyaAmiri@users.noreply.github.com>
2022-03-13 13:20:05 +0330

    Add files via upload

---
+++ exception fasle 3/kar dar class/src/com/company/formSabteNam.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class formSabteNam extends Exception{
+
+    public void name(){
+        Scanner name=new Scanner(System.in);
+        ArrayList<String> nam=new ArrayList<>();
+        System.out.println("enter your name");
+        nam.add(name.next());
+
+    }
+}
