fc86be0a7ebad0f40d0b82dfc2b8d0eddd5cb615
AmirPouyaAmiri <94058945+AmirPouyaAmiri@users.noreply.github.com>
2022-03-13 13:24:51 +0330

    Add files via upload

---
+++ exception fasle 4/tamrin 2/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) throws Moadele {
+        Moadele moadele=new Moadele();
+        moadele.zarib();
+    }
+}
